12d0ec75f2cd99ee553f80677b42c55a4a6e2d84
Mikk Laansalu <miklaa@taltech.ee>
2025-02-12 19:07:55 +0200

    123

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import zipfile
+
+for i in range (10,200):
+    filename = "file%d" %i
+    f = open('random_files/'+ filename, 'rb').read(3)
+    if f == (b'\xff\xd8\xff'):
+        os.rename('random_files/' + filename, 'random_files/' + filename + '.jpg')
+        print("jpeg found")
+    else:
+        os.remove('random_files/' + filename)
